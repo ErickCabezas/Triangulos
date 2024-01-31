2fe4291945f4f0c68e2cdf64beb0807dc9c2a682
ErickCabezas <erick.cabezas@epn.edu.ec>
2024-01-31 15:28:02 -0500

    metodos de tipo de triangulo y area

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -14,6 +14,29 @@ public class Triangulo {
         return ladoa+ladob>ladoc && ladob+ladoc>ladoa && ladoc+ladoa>ladob;
     }
 
+    public String tipoDeTriangulo(){
+        String tipo="Escaleno";
+        if(ladoA==ladoB && ladoB==ladoC){
+            tipo="Triangulo Equilatero";
+            return tipo;
+        }
+        if(ladoA==ladoB || ladoB==ladoC || ladoA==ladoC ){
+            tipo="Triangulo Isósceles";
+            return tipo;
+        }
+        return tipo;
+    }
+
+    public double areaDeTriangulo(){
+        double area=0;
+        double semiperimetro=(ladoA+ladoB+ladoC)/2;
+        area = Math.sqrt(semiperimetro * (semiperimetro - ladoA) * (semiperimetro - ladoB) * (semiperimetro - ladoC));
+        return area;
+    }
+
+
+
+
 
 
 
