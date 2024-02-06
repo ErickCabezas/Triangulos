8d09bcbacbf27bf153bd18665386807ee6dcdb78
ErickCabezas <erick.cabezas@epn.edu.ec>
2024-02-06 09:24:29 -0500

    solo añadí Fecha sdara los registros en el log

--- src/test/TrianguloTest.java
+++ src/test/TrianguloTest.java
@@ -6,6 +6,8 @@ import main.Triangulo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
@@ -14,21 +16,21 @@ public class TrianguloTest {
     public void testEquilatero() {
         Triangulo t = new Triangulo(3, 3, 3);
         assertEquals("Equilatero", t.tipoDeTriangulo());
-        Log.registrarResultado("Resultado testEquilatero: " + t.tipoDeTriangulo());
+        Log.registrarResultado(LocalDate.now()+" Resultado testEquilatero: " + t.tipoDeTriangulo());
     }
 
     @Test
     public void testIsosceles() {
         Triangulo t = new Triangulo(5, 5, 3);
         assertEquals("Isósceles", t.tipoDeTriangulo());
-        Log.registrarResultado("Resultado testIsosceles: " + t.tipoDeTriangulo());
+        Log.registrarResultado(LocalDate.now()+" Resultado testIsosceles: " + t.tipoDeTriangulo());
     }
 
     @Test
     public void testEscaleno() {
         Triangulo t = new Triangulo(3, 4, 5);
         assertEquals("Escaleno", t.tipoDeTriangulo());
-        Log.registrarResultado("Resultado testEscaleno: " + t.tipoDeTriangulo());
+        Log.registrarResultado(LocalDate.now()+" Resultado testEscaleno: " + t.tipoDeTriangulo());
     }
 
     /*test de es triangulo*/
@@ -36,7 +38,7 @@ public class TrianguloTest {
     @Test
     public void testEsTriangulo() throws ExepcionNoEsTriangulo {
         Assert.assertTrue(Triangulo.esTriangulo(3, 4, 5));
-        Log.registrarResultado("Resultado testEsTriangulo: " + Triangulo.esTriangulo(3, 4, 5));
+        Log.registrarResultado(LocalDate.now()+" Resultado testEsTriangulo: " + Triangulo.esTriangulo(3, 4, 5));
     }
 
     @Test
@@ -52,21 +54,21 @@ public class TrianguloTest {
                 = assertThrows(ExepcionNoEsTriangulo.class, () -> Triangulo.esTriangulo(a,b,c));
 
         assertEquals(resultadoEsperado, resultadoObtenido.getMessage());
-        Log.registrarResultado("Resultado testNoEsTriangulo: " + resultadoObtenido.getMessage());
+        Log.registrarResultado(LocalDate.now()+" Resultado testNoEsTriangulo: " + resultadoObtenido.getMessage());
     }
 
     @Test
     public void testArea() {
         Triangulo t = new Triangulo(3, 4, 5);
         assertEquals(6, t.areaDeTriangulo(), 0.1);
-        Log.registrarResultado("Resultado testArea: " + t.areaDeTriangulo());
+        Log.registrarResultado(LocalDate.now()+" Resultado testArea: " + t.areaDeTriangulo());
     }
 
     @Test
     public void testArea2() {
         Triangulo t = new Triangulo(3, 3, 3);
         assertEquals(3.897, t.areaDeTriangulo(), 0.1);
-        Log.registrarResultado("Resultado testArea2: " + t.areaDeTriangulo());
+        Log.registrarResultado(LocalDate.now()+" Resultado testArea2: " + t.areaDeTriangulo());
     }
 
 }
